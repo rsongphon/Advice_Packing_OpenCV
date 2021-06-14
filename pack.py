94b19b9a9b1e6411d51b2c195e059bf24632deb3
rsongphon <83259584+rsongphon@users.noreply.github.com>
2021-06-14 10:56:41 +0700

    Adjust filename and font
    Add valid filename for output video and add timestamp of finish package to video

--- pack.py
+++ pack.py
@@ -28,16 +28,23 @@ def main():
     startRecord = False
 
     while True:
-        # Record Video
+        # Information of order number and staff ID from qr code
         qrRead = {'staff':'11100215XF','order':'DSGA45D'}
-        recVid = recordingVdo(filename = 'test1.avi')
+
+        # Create valid filename: First 4 character of ordernumber follow by date:time
+        filename =  qrRead['order'][:5] + '_'+ str(currentTime['day']) + '_' + currentTime['monthName'] + '_' + str(currentTime['year']) +'.avi'
+        
+        # Return file name and finish time to use to label video filename
+        recVid , finishTime = recordingVdo(filename = filename)
 
         # Cut Footage
+        # Get FPS data duration of the video
         vidData = getDurationFPS(fileInput=recVid)
+        # Cut the duration of video
         vidCutName = cutVideo(fileInput=recVid,videoData=vidData,durTarget=180,mode='cut')
 
-        # Edit, add text , logo
-        videEditName = editVideo(fileInput=vidCutName,id=qrRead,time=currentTime,logoDir=logoDir)
+        # Add text and logo timestamp of finish product
+        vidEditName = editVideo(fileInput=vidCutName,id=qrRead,logoDir=logoDir,timeFinish=finishTime)
 
         
 
@@ -70,6 +77,7 @@ def recordingVdo(filename='output.avi'):
     height = int(capture.get(4))
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     framerate = 17 # Framerate must be matach with the camera. See specification !!
+
     
     
     # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
@@ -83,12 +91,14 @@ def recordingVdo(filename='output.avi'):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     
+    finishTime = getCurrentTime()
     capture.release()
     output.release()
     cv2.destroyAllWindows()
-    return filename
 
-def editVideo(fileInput,id,logoDir,time):
+    return filename, finishTime
+
+def editVideo(fileInput,id,logoDir,timeFinish):
 
     videoName = 'edit.avi'
     # Start Capture raw footage
@@ -105,13 +115,21 @@ def editVideo(fileInput,id,logoDir,time):
     while ret: # ret return True if the frame reading is success. 
 
         # Put the text in image 
-        font = cv2.FONT_HERSHEY_SIMPLEX
+        font = cv2.FONT_HERSHEY_SIMPLEX # adjust font type
         textLocation_staff = (0,frame.shape[0]-10)  # adjust the minus value to move the row
-        textLocation_order = (0,frame.shape[0]-50)  # adjust the minus value to move the row
-        cv2.putText(img = frame,text='Staff ID: '+id['staff'],org=textLocation_staff, 
-                    fontFace=font, fontScale=1, color=(255, 50, 255), thickness=1, lineType =cv2.LINE_AA)
-        cv2.putText(img = frame,text='Order No: '+id['order'],org=textLocation_order,
-                    fontFace=font, fontScale=1, color=(255, 50, 255), thickness= 1, lineType=cv2.LINE_AA)
+        textLocation_order = (0,frame.shape[0]-30)  # adjust the minus value to move the row
+        textLocation_finishTime = (0,frame.shape[0]-50)
+
+        text_staff = 'Staff ID: '+id['staff']
+        text_order = 'Order No: '+id['order']
+        text_finishTime = 'Finish Time {}:{} {} {} {}'.format(timeFinish['hour'],timeFinish['min'],
+                                                        timeFinish['day'],timeFinish['monthName'],timeFinish['year'])
+
+        cv2.putText(img = frame,text=text_staff,org=textLocation_staff, 
+                    fontFace=font, fontScale=0.5, color=(0, 255, 150), thickness=1, lineType =cv2.LINE_AA)
+        cv2.putText(img = frame,text=text_order,org=textLocation_order,
+                    fontFace=font, fontScale=0.5, color=(0, 255, 150), thickness= 1, lineType=cv2.LINE_AA)
+        cv2.putText(img = frame,text=text_finishTime ,org=textLocation_finishTime,fontFace=font, fontScale=0.5, color=(255, 245, 0), thickness= 1, lineType=cv2.LINE_AA)
         
         #Put logo in the image
         logoAbsPath = os.path.join(logoDir,'advice.jpg') # Change logo here
@@ -172,7 +190,9 @@ def cutVideo(fileInput,videoData,durTarget,mode='cut'):
             height = int(capture.get(4))
             fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Define the codec and create VideoWriter 
             framerate = 17 # Same as original video
-            output = cv2.VideoWriter('cut{}min.avi'.format(str(durTarget/60)),fourcc,framerate, (width,height))
+            filename = f'{fileInput}_cut'  # change file name here
+            #output = cv2.VideoWriter('cut{}min.avi'.format(str(durTarget/60)),fourcc,framerate, (width,height))
+            output = cv2.VideoWriter(filename,fourcc,framerate, (width,height))
 
             ret, frame = capture.read() # start reading
             countFrame = 1 # first frame
@@ -187,9 +207,10 @@ def cutVideo(fileInput,videoData,durTarget,mode='cut'):
             
             capture.release()
             output.release()
-            return 'cut{}min.avi'.format(str(durTarget/60)) # change file name here
+
+            return filename
         else:
-            return fileInput # return the same videoname
+            return fileInput # return the original videoname incase video did not change
 
     elif mode =='timeLapse':
         # !!adjust fps to speedup the video!!
@@ -202,8 +223,10 @@ def cutVideo(fileInput,videoData,durTarget,mode='cut'):
             capture = cv2.VideoCapture(fileInput) 
             width = int(capture.get(3))
             height = int(capture.get(4))
-            fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Define the codec and create VideoWriter 
-            output = cv2.VideoWriter('cut{}min.avi'.format(str(durTarget/60)),fourcc,fpsExpect, (width,height))
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Define the codec and create VideoWriter
+            filename = f'{fileInput}_timelapse'  # change file name here
+            #output = cv2.VideoWriter('cut{}min.avi'.format(str(durTarget/60)),fourcc,fpsExpect, (width,height))
+            output = cv2.VideoWriter(filename,fourcc,fpsExpect, (width,height))
 
             ret, frame = capture.read() # start reading
             while ret:
@@ -212,10 +235,10 @@ def cutVideo(fileInput,videoData,durTarget,mode='cut'):
 
             capture.release()
             output.release()
-            return 'cut{}min.avi'.format(str(durTarget/60)) # change file name here
+            return filename 
         
         else:
-            return fileInput # return the same videoname
+            return fileInput # return the original videoname incase video did not change
 
 def addLogo(inputFrame,imgLogo,logoScale=0.1): 
 
